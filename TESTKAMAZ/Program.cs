51c5198f14d974e91fa4d0ad79278116fa096d3f
Фаниль <fanil724@gmail.com>
2024-01-02 13:01:59 +0300

    Add project files.

---
+++ TESTKAMAZ/Program.cs
@@ -0,0 +1,45 @@
+﻿
+List<Employees> emp = ReadEmployees("test.txt");
+if (emp.Count > 0) PrintEmployeeTree(1, 0, emp);
+
+
+void PrintEmployeeTree(int id, int count, List<Employees> employ)
+{
+    if (employ.Count == 0) return;
+    for (int i = 0; i < count; i++) Console.Write("---");
+    Console.WriteLine(employ.First(x => x.id == id).Surname);
+    foreach (Employees emp in employ)
+    {
+        if (emp.idhead == id)
+        {
+            PrintEmployeeTree(emp.id, count + 1, employ);
+        }
+    }
+}
+
+
+List<Employees> ReadEmployees(string path)
+{
+    List<Employees> employees = new List<Employees>();
+    try
+    {
+        using (StreamReader sr = new StreamReader(path))
+        {
+            while (!sr.EndOfStream)
+            {
+                string? line = sr.ReadLine();
+                if (line == null) continue;
+                var str = line.Split("|");
+                employees.Add(new Employees(Convert.ToInt32(str[0]), Convert.ToInt32(str[1]), str[2]));
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ошибка при выполнение  {ex.Message}");
+        return employees;
+    }
+    return employees;
+}
+
+record Employees(int id, int idhead, string Surname);
---
+++ Task 2/Program.cs
@@ -0,0 +1,86 @@
+﻿using Aspose.Cells;
+using System.Xml;
+
+var dir = GetDictionary("ViewerMessages.xlsx");
+
+if (dir.Count > 0)
+{
+    CreateXmlDOC("ViewerMessages.xml", dir);
+}
+
+Dictionary<string, string> GetDictionary(string path)
+{
+    Dictionary<string, string> pairs = new Dictionary<string, string>();
+    try
+    {
+        using (Workbook wb = new Workbook(path))
+        {
+            WorksheetCollection worksheets = wb.Worksheets;
+
+            foreach (var sheet in worksheets)
+            {
+                Worksheet worksheet = sheet;
+                int rows = worksheet.Cells.MaxDataRow;
+                for (int i = 0; i <= rows; i++)
+                {
+                    string? s1 = worksheet.Cells[i, 0].Value.ToString();
+                    string? s2 = worksheet.Cells[i, 1].Value.ToString();
+
+                    if (s1 != null && s2 != null)
+                    {
+                        pairs.Add(s1, s2);
+                    }
+
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ошибка при выполнение  {ex.Message}");
+    }
+
+    return pairs;
+}
+
+
+void CreateXmlDOC(string path, Dictionary<string, string> dir)
+{
+    XmlDocument xmlDocument = new XmlDocument();
+    try
+    {
+        xmlDocument.Load(path);
+        XmlElement? xxml = xmlDocument.DocumentElement;
+        if (xxml != null)
+        {
+            foreach (XmlElement xnode in xxml)
+            {
+                string key = "";
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    var language = childnode.Attributes?.GetNamedItem("language")?.Value;
+
+                    if (language == "en_US")
+                    {
+                        if (dir.ContainsKey(childnode.InnerText))
+                        {
+                            key = childnode.InnerText;
+                        }
+                        else { break; }
+                    }
+                    if (language == "ru_RU")
+                    {
+                        childnode.InnerText = dir[key];
+                    }
+                }
+            }
+        }
+        xmlDocument.Save("ViewerMessagesTest.xml");
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ошибка при выполнение  {ex.Message}");       
+    }
+   
+}
